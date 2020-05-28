52f4d019e5ccac98fc85c5feb5e0eff232bcdbc8
Felipe Secato <felipe.secato@gmail.com>
2020-05-28 17:16:44 -0300

    Array chunk more implementations

--- src/algorithms/arrayChunk/arrayChunk.js
+++ src/algorithms/arrayChunk/arrayChunk.js
@@ -11,4 +11,34 @@ function chunk(array, size) {
   return chunks;
 }
 
-module.exports = chunk;
+// second solution, more optimized without copying the array first
+function chunk2(array, size) {
+  const chunks = [];
+  let start = 0;
+
+  while (start < array.length) {
+    chunks.push(array.slice(start, start + size));
+    start += size;
+  }
+
+  return chunks;
+}
+
+// third solution, just another way
+function chunk3(array, size) {
+  const chunks = [];
+
+  for (const element of array) {
+    const last = chunks[chunks.length - 1];
+
+    if (!last || last.length === size) {
+      chunks.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunks;
+}
+
+module.exports = chunk2;
